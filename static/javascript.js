9ae2501dc69b52a2a4b059e42d6e59345bf41ddf
muzzen <68534325+muzzen@users.noreply.github.com>
2023-12-29 10:09:00 +0530

    Update javascript.js

--- static/javascript.js
+++ static/javascript.js
@@ -1,1 +1,12 @@
+function clearText(){
+  document.querySelector('textarea').value = "" ;
+}
 
+function validSubmission(){
+  var text = document.querySelector('textarea').value;
+  if (text.trim() == ''){
+    alert("Missing the content !!!,<br> Please parse the news article before proceed.");
+    return false;
+  }
+  return true;
+} 
